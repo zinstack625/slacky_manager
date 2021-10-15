a418203982563310640f0f87d88a0a0214c56b07
zinstack625 <klimanov625@outlook.com>
2021-10-16 00:25:56 +0300

    initial commit

---
+++ __main__.py
@@ -0,0 +1,44 @@
+from slack_bolt.async_app import AsyncApp
+from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
+# from logic import *
+import os
+import sqlite3
+
+app = AsyncApp(token=os.environ["SLACK_BOT_TOKEN"])
+count = 0
+
+
+def pull_mentors_from_db():
+    db = sqlite3.connect("cpp-bmstu.db")
+    return db.execute("SELECT TAG FROM mentors").fetchall()
+
+
+@app.command("/ping")
+async def pong(ack, say, command):
+    await ack()
+    print("got a command")
+    await say("Pong")
+
+
+@app.command("/check_me")
+async def request_mentor(ack, respond, command):
+    await ack()
+    lab = command["text"]
+    mentors = pull_mentors_from_db()
+    mentor_tag = mentors[count][0]
+    await respond(f"{lab} was assigned to mentor <@{mentor_tag}>")
+
+
+@app.message("Hello")
+async def hello(message, say):
+    print("boring message")
+    say("hello...")
+
+
+async def main():
+    handler = AsyncSocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
+    await handler.start_async()
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
