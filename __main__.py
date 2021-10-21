450a91313118c2575d75d0ba12a3bcfe1d6b251a
zinstack625 <klimanov625@outlook.com>
2021-10-21 10:59:20 +0300

    url validity on the base of regex

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 from slack_bolt.async_app import AsyncApp
 from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
 import os
+from re import search
 import sqlite3
 from validators import url
 
@@ -17,7 +18,7 @@ def pull_mentors_from_db():
 async def request_mentor(ack, respond, command):
     await ack()
     lab = command["text"][1:-1]
-    if not url(lab):
+    if not url(lab) or search(r'https://github.com/bmstu-cbeer-2021-lab1/.*/pull/[0-9].*', lab) is None:
         await respond("Please send a valid url")
         return
     mentors = pull_mentors_from_db()
@@ -27,12 +28,12 @@ async def request_mentor(ack, respond, command):
     await app.client.conversations_open(users=mentor_tag)
     await app.client.chat_postMessage(
         channel=mentor_tag,
-        text=f"<@{sender_tag}> {lab}"
+        text=f"<@{sender_tag}> <{lab}>"
     )
     db = sqlite3.connect("cpp-bmstu.db")
     count = (
         count + 1) % db.execute("SELECT COUNT(*) FROM mentors").fetchall()[0][0]
-    await respond(f"{lab} was assigned to mentor <@{mentor_tag}>")
+    await respond(f"<{lab}> was assigned to mentor <@{mentor_tag}>")
 
 
 @app.command("/add_mentor")
