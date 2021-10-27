b11b18a676263135b6dade249b6f53da321bf50f
zinstack625 <koala.klimanov@gmail.com>
2021-10-28 00:02:11 +0300

    add direct messages action with checkbox for lab approval

--- __main__.py
+++ __main__.py
@@ -10,6 +10,69 @@ app = AsyncApp(token=os.environ["SLACK_BOT_TOKEN"])
 
 def get_db():
     return sqlite3.connect("/var/db/slackbot/cpp-bmstu.db")
+    # return sqlite3.connect("cpp-bmstu.db")
+
+
+def get_blocks(sender_tag, lab):
+    return [{
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": f"<@{sender_tag}> <{lab}>"
+        },
+        "accessory": {
+            "type": "checkboxes",
+            "action_id": "approved",
+            "options": [{
+                "value": "is_approved",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Approved"
+                }
+            }],
+            "confirm": {
+                "title": {
+                    "type": "plain_text",
+                    "text": "Confirmation"
+                },
+                "text": {
+                    "type": "plain_text",
+                    "text": "Are you sure?"
+                },
+                "confirm": {
+                    "type": "plain_text",
+                    "text": "Yes"
+                },
+                "deny": {
+                    "type": "plain_text",
+                    "text": "No"
+                }
+            }
+        }
+    }]
+
+
+@app.action("approved")
+async def approve_lab(ack, body, payload):
+    await ack()
+    # it's easy enough to pull the id from the request body
+    checker_tag = body["user"]["id"]
+    db = get_db()
+    # let's assume that if loop is not entered, all checkmarks are
+    # disabled, thus lab was unapproved, that's why here it's + 1
+    # over what's been before
+    checker_load = db.execute(
+        "SELECT LOAD FROM mentors WHERE TAG = ?", (checker_tag,)
+    ).fetchone()[0] + 1
+    for i in payload["selected_options"]:
+        if i["value"] == "is_approved":
+            # and here, because of the beforementioned assumption,
+            # we have to substract 2 instead of 1. that's jank tbh
+            checker_load -= 2
+
+    db.execute("UPDATE mentors SET LOAD = ? "
+               "WHERE TAG = ?", (checker_load, checker_tag))
+    db.commit()
 
 
 @app.command("/check_me")
@@ -17,21 +80,28 @@ async def request_mentor(ack, respond, command):
     await ack()
     lab = command["text"][1:-1]
     if not url(lab) or search(
-                r'https://github.com/bmstu-cbeer-2021-lab1/.*'
-                '/pull/[0-9].*', lab) is None:
+        r'https://github.com/bmstu-cbeer-2021-lab1/.*'
+            '/pull/[0-9].*', lab) is None:
         await respond("Please send a valid url")
         return
     db = get_db()
     (mentor_tag, mentor_load) = db.execute(
-            "SELECT TAG FROM mentors ORDER BY LOAD LIMIT 1").fetchone()
-    db.execute(f"UPDATE mentors SET LOAD = {mentor_load + 1}"
-               "WHERE TAG = {mentor_tag}")
+        "SELECT TAG, LOAD FROM mentors ORDER BY LOAD LIMIT 1").fetchone()
+    # even though logically it'd be better to place this after the
+    # designation reply, I want to finish working with the db asap
+    db.execute("UPDATE mentors SET LOAD = ? "
+               "WHERE TAG = ?", (mentor_load + 1, mentor_tag))
     db.commit()
     sender_tag = command["user_id"]
+    # frontend boilerplate
+    mentor_dm_blocks = get_blocks(sender_tag, lab)
     await app.client.conversations_open(users=mentor_tag)
     await app.client.chat_postMessage(
         channel=mentor_tag,
-        text=f"<@{sender_tag}> <{lab}>"
+        blocks=mentor_dm_blocks,
+        # text block is still used for notifications and other places
+        # where it's impossible to show actions
+        text="You've got a new lab to check!"
     )
     await app.client.conversations_open(users=sender_tag)
     await app.client.chat_postMessage(
@@ -52,7 +122,7 @@ async def add_mentor(ack, respond, command):
     mentor_id = command["user_id"]
     if (mentor_id,) not in db.execute("SELECT TAG FROM mentors").fetchall():
         db.execute(
-            f"INSERT INTO mentors VALUES('{mentor_cnt}', '{mentor_id}', 0)")
+            "INSERT INTO mentors VALUES(?, ?, 0)", (mentor_cnt, mentor_id))
         db.commit()
         await respond("Done")
     else:
