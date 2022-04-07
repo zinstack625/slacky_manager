2740f152a0cc6f957e29fa6a9a1eb7d1f61f7fca
zinstack625 <klimanov625@outlook.com>
2022-04-07 17:28:53 +0300

    support mentor deletion with a command

--- slacky_manager/mentor_manage/handles.py
+++ slacky_manager/mentor_manage/handles.py
@@ -18,3 +18,25 @@ async def add_mentor(ack, respond, command):
         await respond("Done")
     else:
         await respond("You're already in!")
+
+
+@slack_app.app.command("/remove_mentor")
+async def remove_mentor(ack, respond, command):
+    await ack()
+    if command["channel_name"] != "private":
+        await respond("Must be called in private chat")
+        return
+    if not dboper.check_admin(command["user_id"]):
+        await respond("You're not the admin")
+        return
+    # mentor_id is expected to be member id and not a mention
+    # if someone can fix that, that'd be nice
+    mentor_id = command["text"]
+    db = dboper.get_db()
+    if db.execute("SELECT EXISTS(SELECT * FROM mentors where TAG=?)",
+                  (mentor_id,)).fetchone() == (1,):
+        db.execute("DELETE FROM mentors WHERE TAG=?", (mentor_id,))
+        db.commit()
+        await respond("Done")
+    else:
+        await respond("There is no such mentor!")
